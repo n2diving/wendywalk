f840c7965c2077723a07280df18f0a141f00695a
Jason <jasonckim10@gmail.com>
2014-07-27 06:01:20 -0700

    map changes

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -4,6 +4,11 @@ class MapsController < ApplicationController
   end
 
   def results
+    @centers = Center.all
+    @centers.each do |center|
+      @lat = (center.lat).to_f
+      @longit = (center.longit).to_f
+    end
   end
 
 end
